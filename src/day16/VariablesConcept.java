384064f9642c698a0ac45484d724d2aff3f39042
Ashish Mishra <ashish.mishra.sse@gmail.com>
2026-09-03 09:47:10 +0530

    Day16: DataTypes & Varaibles Concepts

---
+++ src/day16/VariablesConcept.java
@@ -0,0 +1,46 @@
+package day16;
+
+public class VariablesConcept {
+
+    // What are Variables?
+
+    // I want to store the number 10 --> Create variable
+    // 1. dataType variableName = value;
+
+    // 2. dataType variableName;
+    //    variableName = value;
+
+    // Data Type ---> Byte/short/int/long/float/double/char/boolean
+    int x = 10;
+
+    //I want to store the number 14.56
+
+    double y = 14.56;
+
+    // I want to store the number 100;
+    int z = 100;
+
+    // I want to store the letter 'y'
+
+    char c = 'y';
+
+    // I want to store the value true;
+    boolean b = true;
+
+    // I want to store the letter X
+    char f  = 'X';
+
+    // Data Type ---> Byte/short/long/float/ -- we need to know when to use these data types
+   //nonDecimalNumber ---> [Byte/short/int/long]
+
+    long g = 45;
+
+    //decimalNumber -->[float/double]
+    float h = 123.45F;
+    
+    // How to pick the best data type out of all the nonDecimal and Decimal Numbers
+}
+
+
+
+
